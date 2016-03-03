0405ce6923e406662be4d4f9bf0e8277602cd59e
vu.tran <vu.tran@logigear.com>
2016-03-03 16:30:54 +0700

    121

---
+++ untitled/src/test.java
@@ -0,0 +1,11 @@
+/**
+ * Created by vu.tran on 3/3/2016.
+ */
+public class test {
+    public static void main(String[] args) {
+        // write your code here
+
+        System.out.print("Tran Dinh Vu");
+
+    }
+}
