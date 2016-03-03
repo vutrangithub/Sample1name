b02e80f946beb7f3e057df2c35017399f28a87fb
vu.tran <vu.tran@logigear.com>
2016-03-03 16:54:14 +0700

    edit test class

--- untitled/src/test.java
+++ untitled/src/test.java
@@ -5,7 +5,20 @@ public class test {
     public static void main(String[] args) {
         // write your code here
 
-        System.out.print("Tran Dinh Vu1");
+        System.out.println("Tran Dinh Vu");
+        System.out.println("Tran Dinh Vu");
+        System.out.println("Tran Dinh Vu");
+        System.out.println("Tran Dinh Vu");
+        System.out.println("Tran Dinh Vu");
+        System.out.println("Tran Dinh Vu");
+        System.out.println("Tran Dinh Vu");
+        System.out.println("Tran Dinh Vu");
+        System.out.println("Tran Dinh Vu");
+        System.out.println("Tran Dinh Vu");
+        System.out.println("Tran Dinh Vu");
+        System.out.println("Tran Dinh Vu");
+        System.out.println("Tran Dinh Vu");
+
 
     }
 }
